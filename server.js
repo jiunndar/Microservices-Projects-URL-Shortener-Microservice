1ed32504a6b7d5dc12cba6250ba23b674c83f77b
jiunndar <70994268+jiunndar@users.noreply.github.com>
2021-04-10 17:17:15 +0800

    Update server.js
    code changes

--- server.js
+++ server.js
@@ -1,24 +1,124 @@
 require('dotenv').config();
+
+/** # MONGOOSE SETUP #
+/*  ================== */
+
+/** 1) Install & Set up mongoose */
+const mongoose = require('mongoose');
+
+
+
 const express = require('express');
-const cors = require('cors');
+const mongo = require('mongodb');
+
+const dns = require('dns');
+const URL = require('url').URL;  // a module URL has utilities for URL resolution and parsing
 const app = express();
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
-
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that API is remotely testable by FCC 
+let cors = require('cors');
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+// basic configuration 
+let port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
+
+mongoose.connect(process.env.MONGO_URI);
+
+
+// connect to the DB
+let db = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
+  if (error) console.log(error);
+    console.log("connection to the DB successful");
+});
+
+// create a new Schema with following schema types
+const urlSchema = new mongoose.Schema({
+  originalURL: String,
+  shortenedURL: String
+}, {timestamps: true});
+
+// create a model that allows to create instance of objects--documents
+const Model = mongoose.model('shortURL', urlSchema);
+module.exports = Model;
+
+// this project needs to parse POST bodies--the body-parser mounted here
+let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: false}));  // configuration option to use the classic encoding
+app.use(bodyParser.json())
+
+// to serve the static CSS file from the public folder by using the 
+// built-in middleware function in Express
+app.use('/', express.static(process.cwd() + '/public'));
+
+// routing--how the app responds to a client request to a particular endpoint
+// when the route is matched, the handler function is executed--responds with the index.html file
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+function urlIsValid(url) {
+ return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test( url );
+}
+
+
+app.post('/api/shorturl/new', (req, res, next) => {
+  const originalURL = req.body.url;
+
+  if (!urlIsValid(originalURL)) {
+    res.json({ "error": "URL invalid" });
+
+  } else {
+
+    const urlObject = new URL(originalURL);
+    dns.lookup(urlObject.hostname, (err,address,family) =>{
+      if (err){
+        res.json({
+          originalURL : originalURL,
+          shortenedURL: "Invalid URL"
+        });
+
+      } else {
+        let shortenedURL = Math.floor(Math.random()*100000).toString();
+
+        let data = new Model({
+          originalURL: originalURL,
+          shortenedURL: shortenedURL
+        });
+
+        data.save((err, data) =>{
+          if (err) {
+            console.error(err);
+          }
+        });
+
+        res.json({
+          originalURL: originalURL,
+          shortenedURL: shortenedURL
+        })
+      }
+    })
+  }
+
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+
+app.get('/api/shorturl/:urlToForward', (req, res, next) => {
+  let shortenedURL = req.params.urlToForward;
+  
+  Model.findOne({shortenedURL: shortenedURL}, (err, data) => {
+    if (err) {
+      res.send("Error reading database.")
+    }
+    res.redirect(301, data.originalURL);
+  });
 });
+
+// listen for requests
+app.listen(port, () => {
+  console.log('Node.js listening ...');
+});
+
+
